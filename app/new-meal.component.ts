7328eae54abd1afc792c49848657e757173550b3
russvetsper <russvetsper@gmail.com>
2016-08-26 13:42:30 -0700

    add @component to new-meal.component file

--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -1,2 +1,9 @@
 import { Component,EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model'
+
+
+@Component({
+  selector: 'add-meal',
+  outputs: ['onSubmitNewMeal'],
+  templateUrl: '/app/new-meal.component.html'
+})
