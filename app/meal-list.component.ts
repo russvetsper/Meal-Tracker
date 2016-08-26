dad097c31d1a055e4a857802cfee3cb08be5409b
russvetsper <russvetsper@gmail.com>
2016-08-26 12:53:06 -0700

    add Component meal-list

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,2 +1,17 @@
 import { Component } from 'angular2/core';
-import { Skeleton } from './meal.model';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'meal-list',
+  inputs: ['mealList'],
+  template: `
+  <div *ngFor="#currentMeal of mealList" (click)="MealClicked(currentMeal)">
+    <h3>{{currentMeal.name}} | {{currentMeal.description}} | {{currentMeal.calories}}</h3>
+    <form *ngIf="currentMeal === selectedMeal">
+      <input [(ngModel)]="currentMeal.name">
+      <input [(ngModel)]="currentMeal.description">
+      <input [(ngModel)]="currentMeal.calories">
+    </form>
+  </div>
+  `
+})
